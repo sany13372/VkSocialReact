d173fac67ef7cda209239b3ffe38a3df151e68aa
aleksandr <kuzinalexandar@yandex.ru>
2023-05-15 16:21:24 +0300

    change bag

--- src/context/api/api.ts
+++ src/context/api/api.ts
@@ -1,11 +1,10 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
-const API_URL = 'http://localhost:4200/api'
 export const api = createApi({
     reducerPath:'api',
     tagTypes:['Dialogs','User'],
     baseQuery:fetchBaseQuery({
-        baseUrl:API_URL,
+        baseUrl:`${import.meta.env.VITE_REACT_API_URL}`
     }),
     endpoints:() => ({})
 })
